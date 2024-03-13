e807540e0450f5e59faa474d512016edfaa414ae
ziyang <791191656@qq.com>
2024-03-12 17:39:58 -0700

    remove none-ls, change default diagnostic icon

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,7 +1,7 @@
 local on_attach = require("nvchad.configs.lspconfig").on_attach
 local capabilities = require("nvchad.configs.lspconfig").capabilities
 
-local lspconfig = require "lspconfig"
+local lspconfig = require("lspconfig")
 
 -- if you just want default config for the servers then put them in a table
 local servers = { "bashls", "pyright", "clangd", "r_language_server", "texlab", "ltex" }
@@ -10,44 +10,44 @@ local servers = { "bashls", "pyright", "clangd", "r_language_server", "texlab", 
 -- vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
 -- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, { border = "rounded" })
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
+	lspconfig[lsp].setup({
+		on_attach = on_attach,
+		capabilities = capabilities,
+	})
 end
 
-local cmp_nvim_lsp = require "cmp_nvim_lsp"
+local cmp_nvim_lsp = require("cmp_nvim_lsp")
 
-require("lspconfig").r_language_server.setup {
-  handlers = {
-    ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-      -- Disable virtual_text
-      virtual_text = { severity = { min = vim.diagnostic.severity.ERROR } },
-      signs = { severity = { min = vim.diagnostic.severity.ERROR } },
-      underline = { severity = { min = vim.diagnostic.severity.ERROR } },
-    }),
-  },
-}
+require("lspconfig").r_language_server.setup({
+	handlers = {
+		["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+			-- Disable virtual_text
+			virtual_text = { severity = { min = vim.diagnostic.severity.ERROR } },
+			signs = { severity = { min = vim.diagnostic.severity.ERROR } },
+			underline = { severity = { min = vim.diagnostic.severity.ERROR } },
+		}),
+	},
+})
 
-require("lspconfig").clangd.setup {
-  on_attach = on_attach,
-  capabilities = cmp_nvim_lsp.default_capabilities(),
-  ["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
-    -- Disable virtual_text
-    source = true,
-  }),
-  cmd = {
-    "clangd",
-    "--offset-encoding=utf-16",
-  },
-}
+require("lspconfig").clangd.setup({
+	on_attach = on_attach,
+	capabilities = cmp_nvim_lsp.default_capabilities(),
+	["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+		-- Disable virtual_text
+		source = true,
+	}),
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+	},
+})
 
-require("lspconfig").ltex.setup {
-  settings = {
-    ltex = {
-      checkFrequency = "save",
-    },
-  },
-}
+require("lspconfig").ltex.setup({
+	settings = {
+		ltex = {
+			checkFrequency = "save",
+		},
+	},
+})
 --
 -- lspconfig.pyright.setup { blabla}
--- lua/myinit.lua
+++ lua/myinit.lua
@@ -16,7 +16,14 @@ vim.o.foldlevelstart = 99
 vim.o.foldenable = true
 vim.o.fillchars = [[eob: ,fold: ,foldopen:󰤻,foldsep: ,foldclose:󰡌]]
 
-vim.o.cursorlineopt = 'number,line'
+-- NOTE: enable cursoreline
+vim.o.cursorlineopt = "number,line"
+
+local symbols = { Error = "󰅙", Info = "󰋼", Hint = "󰌵", Warn = "" }
+for name, icon in pairs(symbols) do
+	local hl = "DiagnosticSign" .. name
+	vim.fn.sign_define(hl, { text = icon, numhl = hl, texthl = hl })
+end
 
 -- NOTE: limit completion items
 vim.opt.pumheight = 10
@@ -25,48 +32,48 @@ vim.opt.pumheight = 10
 vim.wo.relativenumber = true
 
 -- NOTE: remap the :w
-vim.cmd "command! W write"
+vim.cmd("command! W write")
 
 -- NOTE: ignore the smartcase
-vim.cmd [[
+vim.cmd([[
 set ignorecase smartcase
-]]
+]])
 
 -- NOTE: jump out of the bracket
 function EscapePair()
-  local closers = { ")", "]", "}", ">", "'", '"', "`", ",", "$" }
-  local line = vim.api.nvim_get_current_line()
-  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-  local after = line:sub(col + 1, -1)
-  local closer_col = #after + 1
-  local closer_i = nil
-  for i, closer in ipairs(closers) do
-    local cur_index, _ = after:find(closer)
-    if cur_index and (cur_index < closer_col) then
-      closer_col = cur_index
-      closer_i = i
-    end
-  end
-  if closer_i then
-    vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
-  else
-    vim.api.nvim_win_set_cursor(0, { row, col + 1 })
-  end
+	local closers = { ")", "]", "}", ">", "'", '"', "`", ",", "$" }
+	local line = vim.api.nvim_get_current_line()
+	local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+	local after = line:sub(col + 1, -1)
+	local closer_col = #after + 1
+	local closer_i = nil
+	for i, closer in ipairs(closers) do
+		local cur_index, _ = after:find(closer)
+		if cur_index and (cur_index < closer_col) then
+			closer_col = cur_index
+			closer_i = i
+		end
+	end
+	if closer_i then
+		vim.api.nvim_win_set_cursor(0, { row, col + closer_col })
+	else
+		vim.api.nvim_win_set_cursor(0, { row, col + 1 })
+	end
 end
 vim.api.nvim_set_keymap("i", "<C-d>", "<cmd>lua EscapePair()<CR>", { noremap = true, silent = true })
 
 -- NOTE: neovide
 if vim.g.neovide then
-  -- Put anything you want to happen only in Neovide here
-  vim.o.guifont = "DankMono Nerd Font:h12"
-  vim.g.neovide_transparency = 1
-  vim.g.neovide_cursor_animation_length = 0.1
-  vim.g.neovide_cursor_trail_size = 0.5
-  vim.g.neovide_cursor_vfx_mode = "Railgun"
-  vim.g.neovide_padding_top = 0
-  vim.g.neovide_padding_bottom = 0
-  vim.g.neovide_padding_right = 0
-  vim.g.neovide_padding_left = 0
-  vim.g.neovide_remember_window_size = true
-  vim.g.neovide_refresh_rate = 80
+	-- Put anything you want to happen only in Neovide here
+	vim.o.guifont = "DankMono Nerd Font:h12"
+	vim.g.neovide_transparency = 1
+	vim.g.neovide_cursor_animation_length = 0.1
+	vim.g.neovide_cursor_trail_size = 0.5
+	vim.g.neovide_cursor_vfx_mode = "Railgun"
+	vim.g.neovide_padding_top = 0
+	vim.g.neovide_padding_bottom = 0
+	vim.g.neovide_padding_right = 0
+	vim.g.neovide_padding_left = 0
+	vim.g.neovide_remember_window_size = true
+	vim.g.neovide_refresh_rate = 80
 end
