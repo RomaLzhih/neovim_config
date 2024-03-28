6fd47781d5c2953248e079234cb56f5630b43be8
ziyang <791191656@qq.com>
2024-03-28 15:27:21 -0700

    update noice

--- lua/configs/noice.lua
+++ lua/configs/noice.lua
@@ -6,6 +6,12 @@ require("noice").setup({
 			["vim.lsp.util.stylize_markdown"] = true,
 			["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
 		},
+		signature = {
+			enabled = false,
+		},
+		hover = {
+			enabled = false,
+		},
 	},
 	-- you can enable a preset for easier configuration
 	presets = {
