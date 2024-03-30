958eb35b3a9ca7b1ee34904965c10471dd076a2f
RomaLzhih <791191656@qq.com>
2024-03-30 14:41:08 -0700

    update alpha-nvim

--- lua/configs/alpha-nvim.lua
+++ lua/configs/alpha-nvim.lua
@@ -17,24 +17,25 @@ dashboard.section.header.val = {
 local function button(sc, txt, keybind, keybind_opts)
 	local b = dashboard.button(sc, txt, keybind, keybind_opts)
 	b.opts.hl = "AlphaButtons"
-	b.opts.hl_shortcut = "AlphaShortcut"
+	b.opts.hl_shortcut = "italic"
 	return b
 end
 
 -- Set menu
 dashboard.section.buttons.val = {
-	button("u", "  > Update plugins", "<cmd> Lazy update<CR>"),
 	button("f", "󰮗  > Find file", "<cmd> Telescope find_files<CR>"),
-	button("r", "  > Recent", ":Telescope oldfiles<CR>"),
+	button("r", "  > Recent", "<cmd> Telescope oldfiles<CR>"),
+	button("i", "󱓷  > Wiki", "<cmd> Neorg index<CR>"),
+	button("u", "  > Update plugins", "<cmd> Lazy update<CR>"),
 	button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h | split . | wincmd k | pwd<CR>"),
-	button("q", "󰩈  > Quit NVIM", ":qa<CR>"),
+	button("q", "󰩈  > Quit NVIM", "<cmd> qa<CR>"),
 }
 
 dashboard.section.footer.val = fortune()
 
-dashboard.section.header.opts.hl = "AlphaHeader"
+dashboard.section.header.opts.hl = "italic"
 dashboard.section.buttons.opts.hl = "AlphaButtons"
-dashboard.section.footer.opts.hl = "AlphaFooter"
+dashboard.section.footer.opts.hl = "italic"
 
 -- Send config to alpha
 alpha.setup(dashboard.opts)
