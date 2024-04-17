cc057f1214675027d1c379ae61c4c95f066f3c8c
RomaLzhih <791191656@qq.com>
2024-04-16 17:16:18 -0700

    remove spider

--- lua/plugins/myplugins.lua
+++ lua/plugins/myplugins.lua
@@ -41,17 +41,6 @@ local plugins = {
 		lazy = false,
 	},
 
-	-- NOTE: nvim-spider
-	{
-		"chrisgrieser/nvim-spider",
-		lazy = true,
-		init = function()
-			vim.keymap.set({ "n", "o", "x" }, "w", "<cmd>lua require('spider').motion('w')<CR>", { desc = "Spider-w" })
-			vim.keymap.set({ "n", "o", "x" }, "e", "<cmd>lua require('spider').motion('e')<CR>", { desc = "Spider-e" })
-			vim.keymap.set({ "n", "o", "x" }, "b", "<cmd>lua require('spider').motion('b')<CR>", { desc = "Spider-b" })
-		end,
-	},
-
 	-- NOTE: the diffview plugin
 	{
 		"sindrets/diffview.nvim",
