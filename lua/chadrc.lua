26bdb13334da6e893da4da0d28a7cc3312cba44f
ziyang <791191656@qq.com>
2024-07-04 13:36:48 -0700

    restore default better escape

--- lua/chadrc.lua
+++ lua/chadrc.lua
@@ -5,8 +5,8 @@ local M = {}
 local highlights = require("highlights")
 
 M.ui = {
-	theme = "myGruvbox",
-	theme_toggle = { "myGruvbox", "myGruvbox" },
+	theme = "myRosePine",
+	theme_toggle = { "myRosePine", "myRosePine" },
 	hl_override = highlights.override,
 	hl_add = highlights.add,
 	statusline = {
