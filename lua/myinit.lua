de574d59dfcf4ec46d8fc2dcb7e46c4c3c461955
ziyang <791191656@qq.com>
2024-08-27 14:56:51 -0700

    update neovide config

--- lua/myinit.lua
+++ lua/myinit.lua
@@ -119,6 +119,9 @@ if vim.g.neovide then
 	vim.o.guifont = "JetBrainsMono NF:h12"
 	-- vim.o.guifont = "Consolas ligaturized v3:h13"
 	vim.g.neovide_transparency = 0.95
+	vim.g.neovide_cursor_vfx_mode = "railgun"
+	vim.g.neovide_cursor_animate_command_line = false
+	vim.g.neovide_hide_mouse_when_typing = true
 	vim.g.neovide_cursor_animation_length = 0.1
 	vim.g.neovide_cursor_trail_size = 0.5
 	vim.g.neovide_padding_top = 0
