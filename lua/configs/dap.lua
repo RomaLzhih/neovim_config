c6a49e392355718449343c3c0a4c1db4101f1c9f
ziyang <791191656@qq.com>
2024-11-18 22:43:16 -0800

    feat: update configurations for lua and dap
    - Modified `lua/chadrc.lua` to improve settings
    - Updated `lua/configs/dap.lua` for better debugging support

--- lua/configs/dap.lua
+++ lua/configs/dap.lua
@@ -1,17 +1,47 @@
 local dap = require("dap")
+local bin_path = ""
+local args_string = ""
 dap.configurations.cpp = {
 	{
-		name = "Launch file",
+		name = "Launch codelldb",
 		type = "codelldb",
 		request = "launch",
 		program = function()
-			return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			if bin_path == "" then
+				bin_path = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+			end
+			return bin_path
 		end,
 		cwd = "${workspaceFolder}",
 		stopOnEntry = false,
 		args = function()
-			local args_string = vim.fn.input("Arguments: ")
+			if args_string == "" then
+				args_string = vim.fn.input("Arguments: ")
+			end
 			return vim.split(args_string, " ")
+		end,
+		preLaunchTask = function()
+			local build_command = "make -C" .. bin_path -- Replace with your build command
+			local result = os.execute(build_command)
+			if result ~= 0 then
+				print("Build failed!")
+				return false
+			end
+			return true
 		end,
 	},
 }
+
+function set_debug_command_arg()
+	bin_path = vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/", "file")
+	args_string = vim.fn.input("Arguments: ")
+	print("bin_path: " .. bin_path)
+	print("args_string: " .. args_string)
+end
+
+vim.keymap.set(
+	"n",
+	"<leader>dt",
+	":lua set_debug_command_arg()<CR>",
+	{ noremap = true, silent = true, desc = "get new debug target" }
+)
