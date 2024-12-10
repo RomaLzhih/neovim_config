8f03b2a0208174bdc4c8816850e9cf253189c167
ziyang <791191656@qq.com>
2024-12-09 16:56:44 -0800

    feat(plugins): add treesitter based incremental selection
    Added nvim-treesitter-textsubjects plugin to enable treesitter based
    incremental selection. Configured keymaps for smart selection and
    container selection.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,6 +6,28 @@ return {
 	-- NOTE: enable nvim copy to system clipboard
 	{ "ojroques/nvim-osc52", lazy = false },
 
+	-- NOTE: treesitter based incremental selection
+	{
+		"RRethy/nvim-treesitter-textsubjects",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				textsubjects = {
+					enable = true,
+					prev_selection = ",", -- (Optional) keymap to select the previous selection
+					keymaps = {
+						["."] = "textsubjects-smart",
+						[";"] = "textsubjects-container-outer",
+						["i;"] = {
+							"textsubjects-container-inner",
+							desc = "Select inside containers (classes, functions, etc.)",
+						},
+					},
+				},
+			})
+		end,
+		lazy = false,
+	},
+
 	-- NOTE: enable treesitter for text object select
 	{
 		"nvim-treesitter/nvim-treesitter-textobjects",
