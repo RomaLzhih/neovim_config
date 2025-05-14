5ee927c2cdcfc4fa24c2e1642d52367165ad1cab
SwansZiyangswan <ziyang@SwansSwan.local>
2025-05-14 13:29:27 +0800

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -16,6 +16,21 @@ return {
     -- Author: lokesh-krishna
     -- MIT license, see LICENSE for more details.
 
+    local clients_lsp = function()
+      local bufnr = vim.api.nvim_get_current_buf()
+
+      local clients = vim.lsp.get_clients()
+      if next(clients) == nil then
+        return ""
+      end
+
+      local c = {}
+      for _, client in pairs(clients) do
+        table.insert(c, client.name)
+      end
+      return " " .. table.concat(c, "  ")
+    end
+
     local icons = LazyVim.config.icons
 
     vim.o.laststatus = vim.g.lualine_laststatus
@@ -35,6 +50,9 @@ return {
           LazyVim.lualine.root_dir(),
           { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
           { LazyVim.lualine.pretty_path() },
+        },
+        lualine_x = {
+          { clients_lsp },
           {
             "diagnostics",
             symbols = {
@@ -45,7 +63,6 @@ return {
             },
           },
         },
-        lualine_x = {},
         lualine_y = { { "filetype", separator = { right = "║" } }, "progress" },
         lualine_z = {
           { "location", separator = { right = "" }, left_padding = 2 },
