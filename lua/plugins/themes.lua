4fd38373512caff91c376ba747a9aed083d09694
Swans&Swan <791191656@qq.com>
2025-09-02 19:41:16 -0400

    update tokyonight

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -152,7 +152,7 @@ return {
           Bracket = { fg = "#fbf1c7" },
         },
       })
-      vim.cmd("colorscheme rose-pine")
+      vim.cmd("colorscheme tokyonight")
     end,
     priority = 1000,
     lazy = false,
