b2c98d435510107e3be929e5e155907967f81f83
RomaLzhih <791191656@qq.com>
2025-09-17 15:39:58 -0700

    update

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -6,7 +6,17 @@ return {
     input = { enabled = true },
     scroll = {
       enabled = true,
-      spamming = 100,
+      -- spamming = 100,
+      animate = {
+        duration = { step = 15, total = 200 },
+        easing = "linear",
+      },
+      -- faster animation when repeating scroll after delay
+      animate_repeat = {
+        delay = 50, -- delay in ms before using the repeat animation
+        duration = { step = 5, total = 50 },
+        easing = "linear",
+      },
       filter = function(buf)
         return vim.g.snacks_scroll ~= false
           and vim.b[buf].snacks_scroll ~= false
