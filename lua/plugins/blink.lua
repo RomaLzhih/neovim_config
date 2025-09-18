5ef049ed51fbab03d6b9d2f85ee51b329ddb5c4f
RomaLzhih <791191656@qq.com>
2025-09-18 14:48:38 -0700

    add compilation menu

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -23,11 +23,35 @@ return {
     opts = {
       completion = {
         ghost_text = { enabled = false },
+        menu = {
+          draw = {
+            components = {
+              kind_icon = {
+                text = function(ctx)
+                  local kind_icon, _, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return kind_icon
+                end,
+                -- (optional) use highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+              kind = {
+                -- (optional) use highlights from mini.icons
+                highlight = function(ctx)
+                  local _, hl, _ = require("mini.icons").get("lsp", ctx.kind)
+                  return hl
+                end,
+              },
+            },
+          },
+        },
       },
       cmdline = {
-  -- keymap = { preset = 'inherit' },
-  completion = { menu = { auto_show = false } },
-},
+        -- keymap = { preset = 'inherit' },
+        completion = { menu = { auto_show = false } },
+      },
       sources = {
         default = { "copilot", "lsp", "snippets", "path", "buffer" },
         providers = {
