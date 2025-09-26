c925015c944a244b92b02916195b1efd414f011f
RomaLzhih <791191656@qq.com>
2025-09-26 16:30:23 -0700

    update

--- lua/plugins/ai_plugins.lua
+++ lua/plugins/ai_plugins.lua
@@ -50,7 +50,7 @@ return { -- NOTE: avante
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      -- model = "gpt-5",
+      model = "claude-sonnet-4",
       show_folds = false, -- Shows folds for sections in chat
       show_help = true, -- Shows help message as virtual lines when waiting for user input
       auto_insert_mode = false,
