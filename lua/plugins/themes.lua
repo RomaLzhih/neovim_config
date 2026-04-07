136f3225db54b3a1cdb4000f44f9e047e5f00ac0
Swans&Swan <791191656@qq.com>
2026-04-06 20:42:33 -0700

    update

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -10,6 +10,16 @@ return {
   {
     "loctvl842/monokai-pro.nvim",
     opts = {
+      styles = {
+        comment = { italic = false },
+        keyword = { italic = false },
+        type = { italic = false },
+        storageclass = { italic = false },
+        structure = { italic = false },
+        parameter = { italic = false },
+        annotation = { italic = false },
+        tag_attribute = { italic = false },
+      },
       override = function()
         return {
           ["@punctuation.bracket"] = { fg = "#f9ebaf" },
@@ -179,7 +189,7 @@ return {
     config = function()
       local fm = require("fluoromachine")
       fm.setup({
-        glow = true,
+        glow = false,
         brightness = 0.02,
         theme = "retrowave",
         transparent = false,
