ecb4602c8826e065b9f3ff92f234e915e6796fb0
ziyang <791191656@qq.com>
2026-04-27 14:52:40 -0700

    remove tiny inline

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -526,13 +526,15 @@ return {
     "rachartier/tiny-inline-diagnostic.nvim",
     event = "VeryLazy", -- Or `LspAttach`
     priority = 1000, -- needs to be loaded in first
+    enabled = false,
     config = function()
       require("tiny-inline-diagnostic").setup({
         preset = "amongus",
-        show_all_diags_on_cursorline = true,
+        show_all_diags_on_cursorline = false,
+        show_diags_only_under_cursor = true,
         severity = {
           vim.diagnostic.severity.ERROR,
-          vim.diagnostic.severity.WARN,
+          -- vim.diagnostic.severity.WARN,
         },
       })
       vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
