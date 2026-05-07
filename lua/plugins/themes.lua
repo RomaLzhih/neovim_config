6eb46f98ed2043d734f37d7bd61558695348e88a
RomaLzhih <791191656@qq.com>
2026-05-06 23:26:46 -0700

    update

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -1,5 +1,13 @@
 return {
   {
+    "D0nw0r/dark2026.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme("dark2026")
+    end,
+  },
+  {
     "jpwol/thorn.nvim",
     lazy = false,
     priority = 1000,
