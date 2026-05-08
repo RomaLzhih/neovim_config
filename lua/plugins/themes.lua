b922d919b1970cea425b2115f149c63f89e6df58
RomaLzhih <791191656@qq.com>
2026-05-07 21:57:41 -0700

    udpate

--- lua/plugins/themes.lua
+++ lua/plugins/themes.lua
@@ -3,16 +3,15 @@ return {
     "D0nw0r/dark2026.nvim",
     lazy = false,
     priority = 1000,
-    config = function()
-      vim.cmd.colorscheme("dark2026")
-    end,
   },
+
   {
     "jpwol/thorn.nvim",
     lazy = false,
     priority = 1000,
     opts = {},
   },
+
   {
     "everviolet/nvim",
     lazy = true,
