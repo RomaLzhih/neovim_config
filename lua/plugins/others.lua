e993d2fa2ad2d7332a59a751e27ce9d3fcf94ba1
ziyang@mac <791191656@qq.com>
2026-09-03 23:03:42 -0700

    update

--- lua/plugins/others.lua
+++ lua/plugins/others.lua
@@ -289,7 +289,7 @@ return {
   {
     "epwalsh/obsidian.nvim",
     version = "*", -- recommended, use latest release instead of latest commit
-    lazy = true,
+    lazy = false,
     enabled = vim.fn.has("mac") == 1 or vim.fn.has("win32") == 1 or is_ubuntu(),
     dependencies = {
       "nvim-lua/plenary.nvim",
